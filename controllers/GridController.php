184b8eea1146facf328771cc0f4acf859cb96819
xuefeng2 <xuefeng2@kingsoft.com>
2022-06-02 21:42:08 +0800

    feat, add data display

--- controllers/GridController.php
+++ controllers/GridController.php
@@ -2,16 +2,30 @@
 namespace app\controllers;
 
 use app\models\Supplier;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 
 class GridController extends Controller
 {
     public function actionList()
     {
-        $suppliers = Supplier::find()->where(['t_status'=>'ok'])->all();
+        //$suppliers = Supplier::find()->where(['t_status'=>'ok'])->all();
 
-        return $this->render('list', [
-            'models' => $suppliers
+        $where = [];
+        $render = [];
+
+        $render['dataProvider'] = new ActiveDataProvider([
+            'query' => Supplier::find()->where($where),
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_ASC,
+                ]
+            ],
         ]);
+
+        return $this->render('list', $render);
     }
 }
--- views/grid/list.php
+++ views/grid/list.php
@@ -3,10 +3,40 @@
 /** @var yii\web\View $this */
 
 use yii\web\View;
+use \yii\grid\GridView;
 
 $this->title = 'GridView Demo';
 
-print_r($models);
 
-?>
 
+
+
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            // ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            [
+                'label' => '姓名',
+                'value' => function ($model) {
+                    return $model->name;
+                }
+            ],
+            [
+                'label' => '编码',
+                'value' => function ($model) {
+                    return $model->code;
+                }
+            ],
+            [
+                'label' => '状态',
+                'value' => function ($model) {
+                    return $model->t_status;
+                }
+            ],
+        ],
+    ]);
+
+
+
+    ?>
